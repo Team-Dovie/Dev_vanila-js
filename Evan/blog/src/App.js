8affe741f60a00091bf6f5e5848401543c1378ee
sanghoho <ash1151@ajou.ac.kr>
2021-06-25 00:56:36 +0900

    Update: Course4-Event complete

--- Evan/blog/src/App.js
+++ Evan/blog/src/App.js
@@ -2,18 +2,24 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// eslint-disable-next-line 
+
 function App() {
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '강남 고기 맛집', '남자 구두 추천' ])
+  let [따봉, 따봉변경] = useState(0);
   let posts = '강남 고기 맛집';
 
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
-        
+
       </div>
       <div className="list">
-        <h3>{ 글제목[0] }</h3>
+        <h3>
+          { 글제목[0] }
+          <span onClick={ () => { 따봉변경(따봉 + 1) } }>👍</span> { 따봉 }
+        </h3>
         <p>2월 17일 발행</p>
         <hr />
 
